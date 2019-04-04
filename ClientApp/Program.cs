15808c3065c86f7e5f9045844c9ee5263f7e62be
muradheydarov <muradheyderov@gmail.com>
2019-04-05 00:40:03 +0400

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static string ip = "127.0.0.1";
+        static int port = 8088;
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                string message = "";
+                socket.Connect(endPoint);
+
+                while (message != "Exit")
+                {
+                    Console.Write("Write your message: ");
+                    message = Console.ReadLine();
+
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    socket.Send(data);
+
+                    int bytes = 0;
+                    byte[] receiveData = new byte[256];
+                    StringBuilder receivedMessage = new StringBuilder();
+
+                    do
+                    {
+                        bytes = socket.Receive(receiveData);
+                        receivedMessage.Append(Encoding.UTF8.GetString(receiveData, 0, bytes));
+                    } while (socket.Available > 0);
+
+                    Console.WriteLine($"Reply from server: {receivedMessage.ToString()}");
+                }
+                
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Connection closed...");            
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static string ip = "127.0.0.1";
+        static int port = 8088;
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(endPoint);
+
+                socket.Listen(10);
+
+                Console.WriteLine("Server started... Waiting for connections...");
+
+                Socket handler = socket.Accept();
+                Console.WriteLine("Client is connected");
+
+                StringBuilder stringBuilder = new StringBuilder("test");
+
+                while (stringBuilder.ToString() != "Exit")
+                {
+                    int bytes = 0;
+                    stringBuilder = new StringBuilder();
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        stringBuilder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    } while (handler.Available > 0);
+
+                    Console.WriteLine($"{DateTime.Now.ToString()} : {stringBuilder.ToString()}");
+
+
+                    string returnMessage = "Message delivered";
+                    byte[] messageBytes = Encoding.UTF8.GetBytes(returnMessage);
+
+                    handler.Send(messageBytes);                    
+                }
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
